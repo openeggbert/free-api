fa415040a2bedf598b52605b01856af12a53f3a2
Robert Vokac <robertvokac@robertvokac.com>
2026-04-21 21:02:06 +0200

    Changes

--- include/windows.h
+++ include/windows.h
@@ -22,6 +22,8 @@ void WINAPI OutputDebugStringW(LPCWSTR lpOutputString);
 #endif
 
 // WinUser.h (Subset)
+typedef WORD ATOM;
+
 typedef struct tagRECT {
     LONG left;
     LONG top;
@@ -43,8 +45,83 @@ typedef struct tagMSG {
     POINT  pt;
 } MSG, *PMSG, *LPMSG;
 
+typedef LRESULT(CALLBACK* WNDPROC)(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+
+typedef struct tagWNDCLASSA {
+    UINT style;
+    WNDPROC lpfnWndProc;
+    int cbClsExtra;
+    int cbWndExtra;
+    HINSTANCE hInstance;
+    HICON hIcon;
+    HCURSOR hCursor;
+    HBRUSH hbrBackground;
+    LPCSTR lpszMenuName;
+    LPCSTR lpszClassName;
+} WNDCLASSA, *PWNDCLASSA, *LPWNDCLASSA;
+
+#define WM_NULL 0x0000
+#define WM_CLOSE 0x0010
+#define WM_DESTROY 0x0002
+#define WM_QUIT 0x0012
+
+#define PM_NOREMOVE 0x0000
+#define PM_REMOVE 0x0001
+
+#define SW_HIDE 0
+#define SW_SHOW 5
+
+#define WS_OVERLAPPEDWINDOW 0x00CF0000L
+#define WS_VISIBLE 0x10000000L
+
+ATOM WINAPI RegisterClassA(const WNDCLASSA* lpWndClass);
+HWND WINAPI CreateWindowExA(DWORD dwExStyle,
+                            LPCSTR lpClassName,
+                            LPCSTR lpWindowName,
+                            DWORD dwStyle,
+                            int X,
+                            int Y,
+                            int nWidth,
+                            int nHeight,
+                            HWND hWndParent,
+                            HMENU hMenu,
+                            HINSTANCE hInstance,
+                            LPVOID lpParam);
+BOOL WINAPI DestroyWindow(HWND hWnd);
+BOOL WINAPI ShowWindow(HWND hWnd, int nCmdShow);
+BOOL WINAPI UpdateWindow(HWND hWnd);
+BOOL WINAPI PeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax, UINT wRemoveMsg);
+BOOL WINAPI TranslateMessage(const MSG* lpMsg);
+LRESULT WINAPI DispatchMessageA(const MSG* lpMsg);
+LRESULT WINAPI DefWindowProcA(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+void WINAPI PostQuitMessage(int nExitCode);
+
+typedef int(WINAPI* FREE_API_WINMAIN_PROC)(HINSTANCE, HINSTANCE, LPSTR, int);
+int WINAPI FreeApiRunWinMain(FREE_API_WINMAIN_PROC entryPoint, int argc, char** argv);
+
+#define RegisterClass RegisterClassA
+#define CreateWindowEx CreateWindowExA
+#define PeekMessage PeekMessageA
+#define DispatchMessage DispatchMessageA
+#define DefWindowProc DefWindowProcA
+
 #ifdef __cplusplus
 }
+
+#if !defined(_WIN32)
+/**
+ * Defines a portable entry point bridge from WinMain style to main.
+ *
+ * Usage:
+ * FREE_API_IMPLEMENT_WINMAIN() {
+ *     // WinMain body
+ * }
+ */
+#define FREE_API_IMPLEMENT_WINMAIN() \
+    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow); \
+    int main(int argc, char** argv) { return FreeApiRunWinMain(&WinMain, argc, argv); } \
+    int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+#endif
 #endif
 
 #endif // FREE_API_WINDOWS_H
--- src/winapi.cpp
+++ src/winapi.cpp
@@ -1,7 +1,87 @@
 #include "windows.h"
-#include <thread>
+#include <SDL3/SDL.h>
+
 #include <chrono>
 #include <cstdio>
+#include <queue>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
+namespace {
+    std::unordered_map<std::string, WNDPROC> g_registeredClasses;
+    std::unordered_map<HWND, WNDPROC> g_windowProcedures;
+    std::queue<MSG> g_messageQueue;
+    bool g_videoInitialized = false;
+
+    bool EnsureVideoSubsystem()
+    {
+        if (g_videoInitialized) {
+            return true;
+        }
+
+        if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+            return false;
+        }
+
+        g_videoInitialized = true;
+        return true;
+    }
+
+    HWND FindWindowById(const SDL_WindowID windowId)
+    {
+        for (const auto& [hwnd, proc] : g_windowProcedures) {
+            auto* sdlWindow = reinterpret_cast<SDL_Window*>(hwnd);
+            if (sdlWindow && SDL_GetWindowID(sdlWindow) == windowId) {
+                return hwnd;
+            }
+        }
+        return NULL;
+    }
+
+    void PushMessage(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+    {
+        MSG msg{};
+        msg.hwnd = hwnd;
+        msg.message = message;
+        msg.wParam = wParam;
+        msg.lParam = lParam;
+        msg.time = static_cast<DWORD>(SDL_GetTicks());
+        msg.pt = {0, 0};
+        g_messageQueue.push(msg);
+    }
+
+    void PumpSdlEvents()
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    PushMessage(NULL, WM_QUIT, 0, 0);
+                    break;
+                case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
+                    PushMessage(FindWindowById(event.window.windowID), WM_CLOSE, 0, 0);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    std::string BuildCommandLine(const int argc, char** argv)
+    {
+        std::string cmdLine;
+        for (int i = 1; i < argc; ++i) {
+            if (!cmdLine.empty()) {
+                cmdLine += ' ';
+            }
+            if (argv[i]) {
+                cmdLine += argv[i];
+            }
+        }
+        return cmdLine;
+    }
+}
 
 extern "C" {
 
@@ -33,6 +113,176 @@ void WINAPI OutputDebugStringW(LPCWSTR lpOutputString) {
             printf("%c", (char)*lpOutputString++);
         }
     }
+}
+
+ATOM WINAPI RegisterClassA(const WNDCLASSA* lpWndClass)
+{
+    if (!lpWndClass || !lpWndClass->lpszClassName || !lpWndClass->lpfnWndProc) {
+        return 0;
+    }
+
+    g_registeredClasses[lpWndClass->lpszClassName] = lpWndClass->lpfnWndProc;
+    return 1;
+}
+
+HWND WINAPI CreateWindowExA(const DWORD dwExStyle,
+                            LPCSTR lpClassName,
+                            LPCSTR lpWindowName,
+                            const DWORD dwStyle,
+                            const int X,
+                            const int Y,
+                            const int nWidth,
+                            const int nHeight,
+                            HWND hWndParent,
+                            HMENU hMenu,
+                            HINSTANCE hInstance,
+                            LPVOID lpParam)
+{
+    (void)dwExStyle;
+    (void)dwStyle;
+    (void)X;
+    (void)Y;
+    (void)hWndParent;
+    (void)hMenu;
+    (void)hInstance;
+    (void)lpParam;
+
+    if (!lpClassName) {
+        return NULL;
+    }
+
+    const auto classIt = g_registeredClasses.find(lpClassName);
+    if (classIt == g_registeredClasses.end()) {
+        return NULL;
+    }
+
+    if (!EnsureVideoSubsystem()) {
+        return NULL;
+    }
+
+    const int width = nWidth > 0 ? nWidth : 640;
+    const int height = nHeight > 0 ? nHeight : 480;
+    const Uint32 flags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE;
+
+    auto* sdlWindow = SDL_CreateWindow(lpWindowName ? lpWindowName : lpClassName, width, height, flags);
+    if (!sdlWindow) {
+        return NULL;
+    }
+
+    HWND hwnd = reinterpret_cast<HWND>(sdlWindow);
+    g_windowProcedures[hwnd] = classIt->second;
+    return hwnd;
+}
+
+BOOL WINAPI DestroyWindow(HWND hWnd)
+{
+    if (!hWnd) {
+        return FALSE;
+    }
+
+    g_windowProcedures.erase(hWnd);
+    SDL_DestroyWindow(reinterpret_cast<SDL_Window*>(hWnd));
+
+    if (g_windowProcedures.empty() && g_videoInitialized) {
+        SDL_QuitSubSystem(SDL_INIT_VIDEO);
+        g_videoInitialized = false;
+    }
+
+    return TRUE;
+}
+
+BOOL WINAPI ShowWindow(HWND hWnd, int nCmdShow)
+{
+    (void)nCmdShow;
+    return hWnd ? TRUE : FALSE;
+}
+
+BOOL WINAPI UpdateWindow(HWND hWnd)
+{
+    return hWnd ? TRUE : FALSE;
+}
+
+BOOL WINAPI PeekMessageA(LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax, UINT wRemoveMsg)
+{
+    (void)hWnd;
+    (void)wMsgFilterMin;
+    (void)wMsgFilterMax;
+
+    if (!lpMsg) {
+        return FALSE;
+    }
+
+    if (g_messageQueue.empty()) {
+        PumpSdlEvents();
+    }
+
+    if (g_messageQueue.empty()) {
+        return FALSE;
+    }
+
+    *lpMsg = g_messageQueue.front();
+    if ((wRemoveMsg & PM_REMOVE) != 0) {
+        g_messageQueue.pop();
+    }
+
+    return TRUE;
+}
+
+BOOL WINAPI TranslateMessage(const MSG* lpMsg)
+{
+    return lpMsg ? TRUE : FALSE;
+}
+
+LRESULT WINAPI DispatchMessageA(const MSG* lpMsg)
+{
+    if (!lpMsg) {
+        return 0;
+    }
+
+    if (lpMsg->message == WM_QUIT) {
+        return 0;
+    }
+
+    const auto it = g_windowProcedures.find(lpMsg->hwnd);
+    if (it != g_windowProcedures.end() && it->second) {
+        return it->second(lpMsg->hwnd, lpMsg->message, lpMsg->wParam, lpMsg->lParam);
+    }
+
+    return DefWindowProcA(lpMsg->hwnd, lpMsg->message, lpMsg->wParam, lpMsg->lParam);
+}
+
+LRESULT WINAPI DefWindowProcA(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+    (void)wParam;
+    (void)lParam;
+
+    if (Msg == WM_CLOSE) {
+        DestroyWindow(hWnd);
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    if (Msg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return 0;
+}
+
+void WINAPI PostQuitMessage(int nExitCode)
+{
+    PushMessage(NULL, WM_QUIT, static_cast<WPARAM>(nExitCode), 0);
+}
+
+int WINAPI FreeApiRunWinMain(FREE_API_WINMAIN_PROC entryPoint, int argc, char** argv)
+{
+    if (!entryPoint) {
+        return -1;
+    }
+
+    std::string commandLine = BuildCommandLine(argc, argv);
+    return entryPoint(NULL, NULL, commandLine.empty() ? NULL : commandLine.data(), SW_SHOW);
 }
 
 }
